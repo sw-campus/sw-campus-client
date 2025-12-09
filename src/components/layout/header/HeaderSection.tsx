874ed0ebab90a1aa6427c2a62a0c4af967d5ccb4
kkkwp <chxrryda@gmail.com>
2025-12-10 01:33:13 +0900

    fix: SSR 렌더링 방지

--- src/components/layout/header/HeaderSection.tsx
+++ src/components/layout/header/HeaderSection.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Header from '@/components/layout/header/Header'
 import Navigation from '@/components/layout/header/NavigationMenu'
 
 export default function HeaderSection() {
   const [open, setOpen] = useState(false)
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => setMounted(true), [])
+  if (!mounted) return null // SSR 단계에서는 아무것도 렌더하지 않음
 
   return (
     <>
