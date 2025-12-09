e1f0a9ab4088b813722c60fde6bfd31e2d9f095b
Me1on <me1on@gmail.com>
2025-12-09 16:37:43 +0900

    review 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 
 import Footer from '@/components/layout/Footer'
-import FooterController from '@/components/layout/FooterController'
 import Header from '@/components/layout/Header'
 import PageContainer from '@/components/layout/PageContainer'
 import QueryClientProviderWrapper from '@/components/providers/query-client-provider'
