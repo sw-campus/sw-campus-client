76942693b496d0e4720f010ce3e5e10c815d49ef
Me1on <me1on@gmail.com>
2025-12-12 11:16:20 +0900

    suspense 수정

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -4,8 +4,14 @@ import VerifyClient from '@/app/auth/verify/VerifyClient'
 
 export default function VerifyPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <p>이메일 인증 확인 중입니다...</p>
-    </div>
+    <Suspense
+      fallback={
+        <div className="flex min-h-screen items-center justify-center">
+          <p>이메일 인증 확인 중입니다...</p>
+        </div>
+      }
+    >
+      <VerifyClient />
+    </Suspense>
   )
 }
