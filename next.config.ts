787996f2706429af124b4f8c87e7cfe52b483719
kyeonghwi <kyeonghwi@gmail.com>
2025-12-15 15:25:07 +0900

    style: fix indentation

--- next.config.ts
+++ next.config.ts
@@ -1,49 +1,49 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    reactCompiler: true,
-    output: 'standalone', // Docker 이미지 최적화를 위한 standalone 모드
-    eslint: {
-        ignoreDuringBuilds: true, // 빌드 시 ESLint 에러 무시
-    },
-    typescript: {
-        ignoreBuildErrors: true, // 빌드 시 TypeScript 에러 무시
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'boottent.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cdn.sayun.studio',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 's3-oneday.s3.ap-northeast-2.amazonaws.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    }
+  /* config options here */
+  reactCompiler: true,
+  output: 'standalone', // Docker 이미지 최적화를 위한 standalone 모드
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 시 ESLint 에러 무시
+  },
+  typescript: {
+    ignoreBuildErrors: true, // 빌드 시 TypeScript 에러 무시
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'boottent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sayun.studio',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-oneday.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default nextConfig
