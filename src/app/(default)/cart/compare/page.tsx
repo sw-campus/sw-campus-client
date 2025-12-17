38819b4862d458f7d90845c0d122f478437cdefe
kkkwp <chxrryda@gmail.com>
2025-12-17 18:31:31 +0900

    style: 비교 영역 표시

--- src/app/(default)/cart/compare/page.tsx
+++ src/app/(default)/cart/compare/page.tsx
@@ -1,5 +1,11 @@
 import CartComparePage from '@/features/cart/components/CartComparePage'
 
 export default function Page() {
-  return <CartComparePage />
+  return (
+    <div className="custom-container">
+      <div className="custom-card">
+        <CartComparePage />
+      </div>
+    </div>
+  )
 }
