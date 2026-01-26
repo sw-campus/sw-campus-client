6edf91cbbc7931e43bcfe923783f6e84b66cbe8d
kkkwp <chxrryda@gmail.com>
2026-01-26 16:00:14 +0900

    메인 배너 이미지 변경

--- src/features/home/components/main-banner.tsx
+++ src/features/home/components/main-banner.tsx
@@ -2,8 +2,15 @@ import Image from 'next/image'
 
 export function MainBanner() {
   return (
-    <section className="relative aspect-[360/432] w-full md:hidden">
-      <Image src="/images/main_banner.png" alt="메인 배너" fill className="object-cover" priority />
-    </section>
+    <>
+      {/* 모바일 배너 */}
+      <section className="relative aspect-360/432 w-full md:hidden">
+        <Image src="/images/main/mobile.png" alt="메인 배너" fill className="object-cover" priority />
+      </section>
+      {/* 데스크톱 배너 */}
+      <section className="relative hidden aspect-1920/600 w-full md:block">
+        <Image src="/images/main/desktop.png" alt="메인 배너" fill className="object-cover" priority />
+      </section>
+    </>
   )
 }
